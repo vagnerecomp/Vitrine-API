1187eb23d2945a4dee783bb92ae81143dd4bbaf8
vagnerecomp <vagnerecomp@gmail.com>
2022-05-02 17:09:43 -0300

    Commit 1: Implemented REST API of products

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -11,7 +11,7 @@ router.post('/', async (req, res)=>{
     }
 
     const product = {
-        name,
+        name: name.charAt(0).toUpperCase() + name.slice(1).toLowerCase(),
         category,
         price
     }
@@ -48,7 +48,64 @@ router.get('/:id', async (req, res)=>{
     }
 })
 
+router.get('/byname/:name', async (req, res)=>{
+    const name = req.params.name;
 
+    try {
+        const product = await Product.findOne({name: name.charAt(0).toUpperCase() + name.slice(1).toLowerCase()});
+        if(!product){
+            res.status(422).json({message: "DB hasn't this product"});
+            return;
+        }
+        res.status(200).json(product);
+    } catch (error) {
+        res.status(500).json({error: error});
+    }
+})
+
+router.patch('/:id', async(req, res)=>{
+    try {
+        const id = req.params.id;
+
+        const {name, category, price} = req.body;
+        
+        const product = {
+            name: !name? null : name.charAt(0).toUpperCase() + name.slice(1).toLowerCase(),
+            category,
+            price
+        }
+
+        const updatedProduct = await Product.updateOne({_id: id}, product);
+
+        if(updatedProduct.matchedCount === 0){
+            res.status(422).json({message: "Product not found!"});
+            return
+        }
+        res.status(200).json({message: "Product updated!", product});
+        
+    } catch (error) {
+        res.status(500).json({error: error});
+    }
+    
+    
+})
+
+router.delete("/:id", async (req, res)=>{
+    const id = req.params.id;
+    const product = await Product.findOne({_id: id});
+    
+    if (!product){
+        res.status(422).json({message: "Product not found"});
+        return
+    }
+    
+    try {
+        await Product.deleteOne({_id: id});
+        res.status(200).json({message: "Product removed"});
+    } catch (error) {
+        res.status(500).json({error: error});        
+    }
+})
 
 
 
