936d7eff7ce200cb40f41e9bfe6e8c654e7c123d
vagnerecomp <vagnerecomp@gmail.com>
2022-05-03 22:22:52 -0300

    Application build

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 require('dotenv').config();
+const port = process.env.PORT || 3000;
 
 //middlewares
 app.use(
@@ -19,7 +20,9 @@ const DB_PASSWORD = encodeURIComponent(process.env.DB_PASSWORD)
 mongoose.connect(`mongodb+srv://${DB_USER}:${DB_PASSWORD}@vitrineapicluster.bboug.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`)
 .then(()=>{
     console.log("MongoDB conected ;)");
-    app.listen(3000);
+    app.listen(port, ()=>{
+        console.info("Application running!")
+    });
 })
 .catch((err)=>{
     console.log(err);
